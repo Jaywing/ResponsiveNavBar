e52f09e41df31720d572479eb0d60228596ae260
James Taylor <james@jtwd.co.uk>
2015-02-11 14:32:52 +0000

    Update jquery.responsive-navbar.js

--- jquery.responsive-navbar.js
+++ jquery.responsive-navbar.js
@@ -21,20 +21,25 @@
  *
  */
 
-$( function() {
-  "use strict";
-  
+(function (win, doc, $) {
+  'use strict';
+
+  if (!doc.querySelector || !win.addEventListener) {
+      console.log('This browser don\'t cut the Mustard - Exiting off-canvas nav');
+      return;
+  }
+
   // init js-nav-bar--responsive
   $('.js-nav-bar--responsive').each(function () { // cycle through each responsive nav-bar on the page
     var $navbar = $(this); // cache this nav-bar
-    
+
     $navbar.find('.nav-bar__trigger').click(function (e) { // enable the nav-bar trigger
       e.preventDefault();
       $navbar.toggleClass('nav-bar--active');
     });
-    
+
     $navbar.addClass('js-nav-bar--ready'); // enable the nav-bar with a "ready" class
-    
+
   });
 
-});
+}(this, this.document, jQuery));
